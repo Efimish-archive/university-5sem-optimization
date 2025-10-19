330a5fe97bb01b89ed63c4010500f4b981ad0cd4
Efim Ishenin <me@efima.ru>
2025-10-19 22:42:03 +0700

    finished 3rd task

--- src/3.py
+++ src/3.py
@@ -1,14 +1,36 @@
 def f(x):
   return (2 * x * (2 * x + 3)) / (x**2 + 4 * x + 5)
 
+# Метод поразрядного поиска
 def bitwise(a, b, epsilon=1e-3):
   h = (b - a) / 4
-  x = a
-  while abs(h) > epsilon:
-    x2 = x + h
-    if f(x) < f(x2):
-      h = -(h / 4)
-    x = x2
-  return x
+  x0 = a
+  steps = 0
+  while True:
+    fx0 = f(x0)
+    x1 = x0 + h
+    fx1 = f(x1)
+    steps += 1
+    if not (a <= x1 <= b):
+      if abs(h) <= epsilon:
+        print(f"За {steps} операций мы нашли f({x1})={fx1}")
+        break
+      else:
+        h = -h/4
+        if x1 <= a:
+          x0 = a
+        else:
+          x0 = b
+        continue
 
-print(bitwise(-2, 1))
+    if fx0 > fx1:
+      x0 = x1
+    else:
+      if abs(h) <= epsilon:
+        print(f"За {steps} операций мы нашли f({x1})={fx1}")
+        break
+      else:
+        x0 = x1
+        h = -h/4
+
+bitwise(-2, 1)
