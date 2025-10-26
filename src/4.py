3fd21bf7379bc98d6e7edf1ae04dd16bd7a3a8fb
Efim Ishenin <me@efima.ru>
2025-10-27 01:01:41 +0700

    finished task 4

--- src/4.py
+++ src/4.py
@@ -1,51 +1,38 @@
-# from math import exp
+import math
+import sympy
 
-# def f(x):
-#   return exp(-2*x) + (x**2)/2
+def f(x):
+  return math.exp(-2*x) + (x**2)/2
 
-# # Метод секущих
-# def secant(a, b, epsilon=1e-3):
-#   pass
+def f_(value):
+  "Производная f'(x)"
+  x = sympy.symbols('x')
+  expr = sympy.exp(-2*x) + (x**2)/2
+  expr = sympy.diff(expr)
+  expr = expr.subs(x, value)
+  return expr.evalf()
 
-# secant(0, 2)
+# Метод секущих
+def secant(a, b, epsilon=1e-3):
+  steps = 0
+  x0 = a
+  x1 = b
+  while abs(f_(x1)) > epsilon:
+    x = x0 - (x1 - x0) * f_(x0) / (f_(x1) - f_(x0))
+    if x < a or x > b:
+      fa, fb = f(a), f(b)
+      if fa <= fb:
+        xmin, fmin = a, fa
+      else:
+        xmin, fmin = b, fb
+      return xmin, fmin, steps
+    x0, x1 = x1, x
+    steps += 1
+  xmin = x1
+  fmin = f(x1)
+  return xmin, fmin, steps
 
-
-# from sympy import symbols,Derivative
-# import sympy
-# from math import exp
-
-# def f_div(value):
-#     x= symbols('x')
-#     expr = sympy.exp(x) + (1/(x+2))
-#     expr=sympy.diff(expr)
-#     expr=expr.subs(x,value).evalf()
-#     return expr
-
-# def f(x):
-#     expr = exp(x)+(1/(x+2))
-#     return expr
-# def hordfunc(a,b,sigm=0.0001):
-#     #1
-#     k=0
-#     ak=a
-#     bk=b
-#     #2
-#     while True:
-#         xk1=ak-(bk-ak)*f_div(ak)/(f_div(bk)-f_div(ak))
-#         fxk1=f_div(xk1)
-#         print(f"{k} |f(Xk+1): {f(xk1)} | f(Xk+1)':{fxk1} | Xk+1:{xk1}")
-#         #3
-#         if abs(fxk1)<=sigm:
-#             return fxk1
-#         #4
-#         else:
-#             if fxk1>0:
-#                 ak1=ak
-#                 bk1=xk1
-#             else:
-#                 ak1=xk1
-#                 bk1=bk
-#             k+=1
-#             ak=ak1
-#             bk=bk1
-# hordfunc(-1,1)
+xmin, fmin, steps = secant(0, 2)
+print(f"x = {xmin}")
+print(f"f(x) = {fmin}")
+print(f"Кол-во шагов = {steps}")
