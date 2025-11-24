464be4befe72b6d648a9b4db4d14bb5f72fc5cb4
Efim Ishenin <me@efima.ru>
2025-11-24 11:35:16 +0700

    improved task 7

--- src/7.py
+++ src/7.py
@@ -6,41 +6,59 @@ x = sp.Symbol('x')
 f = sp.sqrt(x**2 + 5) / (2*x + 3)
 f = sp.lambdify(x, f, 'math')
 
-def golden_ratio(a, b, epsilon=1e-3):
-  "Метод золотого сечения"
-  a0, b0 = a, b
-  alpha = a0 + (3 - math.sqrt(5)) / 2 * (b0-a0)
-  beta = a0 + (math.sqrt(5) - 1) / 2 * (b0-a0)
-  k = 1
-  while True:
-    if f(alpha) <= f(beta):
-      a_current = a0
-      b_current = beta
-      alpha_next = a_current + b_current - alpha
-      beta_next = alpha
-    else:
-      a_current = alpha
-      b_current = b0
-      alpha_next = beta
-      beta_next = a_current + b_current - beta
-    l = b_current - a_current
-    if l <= epsilon:
-      return a_current, (a_current + b_current)/2, b_current
-    k += 1
-    a0 = a_current
-    b0 = b_current
-    alpha = alpha_next
-    beta = beta_next
-
-def golden_ratio_step(a, b):
-  "Один шаг методом золотого сечения"
+def golden_ratio_mod(a, b, epsilon=1e-3):
+  "Модифицированный метод золотого сечения"
   alpha = a + (3 - math.sqrt(5)) / 2 * (b-a)
   beta = a + (math.sqrt(5) - 1) / 2 * (b-a)
-  if f(alpha) <= f(beta):
+  f_alpha = f(alpha)
+  f_beta = f(beta)
+  # Проверяем тройку (a, x_left, x_right)
+  if a < alpha < beta:
+    if f(a) >= f_alpha <= f_beta:
+      return a, alpha, beta
+  # Проверяем тройку (x_left, x_right, b)
+  if alpha < beta < b:
+    if f_alpha >= f_beta <= f(b):
+      return alpha, beta, b
+  # Делаем шаг золотого сечения
+  if f_alpha <= f_beta:
+    # <--- b
     b = beta
+    beta = alpha
+    alpha = a + b - alpha
   else:
+    # a --->
     a = alpha
-  return a, (a+b)/2, b
+    alpha = beta
+    beta = a + b - beta
+
+# def golden_ratio(a, b, epsilon=1e-3):
+#   "Метод золотого сечения"
+#   alpha = a + (3 - math.sqrt(5)) / 2 * (b-a)
+#   beta = a + (math.sqrt(5) - 1) / 2 * (b-a)
+#   while True:
+#     if f(alpha) <= f(beta):
+#       b = beta
+#       alpha_next = a + b - alpha
+#       beta_next = alpha
+#     else:
+#       a = alpha
+#       alpha_next = beta
+#       beta_next = a + b - beta
+#     if b - a <= epsilon:
+#       return a, (a+b)/2, b
+#     alpha = alpha_next
+#     beta = beta_next
+
+# def golden_ratio_step(a, b):
+#   "Один шаг методом золотого сечения"
+#   alpha = a + (3 - math.sqrt(5)) / 2 * (b-a)
+#   beta = a + (math.sqrt(5) - 1) / 2 * (b-a)
+#   if f(alpha) <= f(beta):
+#     b = beta
+#   else:
+#     a = alpha
+#   return a, (a+b)/2, b
 
 def parabolic(a, b, epsilon=1e-3):
   "Метод парабол"
@@ -48,10 +66,9 @@ def parabolic(a, b, epsilon=1e-3):
   f1, f2, f3 = f(x1), f(x2), f(x3)
   k = 1
 
-  while not (x1 < x2 < x3 and f1 >= f2 <= f3) :
-    print('Неравенство 2.9 не выполнитось, делаем итерацию метода золотого сечения')
-    x1, x2, x3 = golden_ratio_step(x1, x3)
-    f1, f2, f3 = f(x1), f(x2), f(x3)
+  if not (x1 < x2 < x3 and f1 >= f2 <= f3) :
+    print('Неравенство 2.9 не выполнитось, используем метод золотого сечения')
+    x1, x2, x3 = golden_ratio_mod(a, b, epsilon)
 
   while True:
     f1, f2, f3 = f(x1), f(x2), f(x3)
@@ -70,13 +87,13 @@ def parabolic(a, b, epsilon=1e-3):
       d = abs(previous_x_ - x_)
       if d <= epsilon:
         print(f"Точка минимума - {x_}")
-        print(f"Значение функции в этой точке - {fx_}")
+        print(f"Значение функции в этой точке - {f(x_)}")
         return
       # Лимит в 1000 итераций
       if k > 1000:
         print("Количество итераций = 1000, останавливаем поиск")
         print(f"Точка минимума - {x_}")
-        print(f"Значение функции в этой точке - {fx_}")
+        print(f"Значение функции в этой точке - {f(x_)}")
         return
 
     # Шаг 4
@@ -85,13 +102,17 @@ def parabolic(a, b, epsilon=1e-3):
     if x2 <= x_ and x_ <= x3 and fx_ <= f2:
       x1 = x2
       x2 = x_
+      print("x2 <= x_ <= x3, f(x_) <= f2", "x1 = x2, x2 = x_", sep="\n")
     elif x2 <= x_ and x_ <= x3 and fx_ > f2:
       x3 = x_
+      print("x2 <= x_ <= x3, f(x_) > f2", "x3 = x_", sep="\n")
     elif x1 <= x_ and x_ <= x2 and fx_ <= f2:
       x3 = x2
       x2 = x_
+      print("x1 <= x_ <= x2, f(x_) <= f2", "x3 = x2, x2 = x_", sep="\n")
     elif x1 <= x_ and x_ <= x2 and fx_ > f2:
       x1 = x_
+      print("x1 <= x_ <= x2, f(x_) > f2", "x1 = x_", sep="\n")
 
     k += 1
     previous_x_ = x_
