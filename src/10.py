c2273365d1e94192a8642d026c87f01f78326d5d
Efim Ishenin <me@efima.ru>
2025-12-29 17:58:39 +0700

    done task 10

--- src/10.py
+++ src/10.py
@@ -3,6 +3,7 @@ import sympy as sp
 
 x1, x2, x3 = sp.symbols('x1,x2,x3')
 
+# функция квадратичная
 f = 3*x1**2 + 5*x2**2 + 4*x3**2 + 2*x1*x2 - x1*x3 - x2*x3 + 7*x1 + x3
 gradient = sp.Matrix([sp.diff(f, x1), sp.diff(f, x2), sp.diff(f, x3)])
 hessian = sp.hessian(f, (x1, x2, x3))
@@ -16,33 +17,31 @@ gradient = sp.lambdify((x1, x2, x3), gradient)
 hessian = np.array(hessian, dtype=float)
 
 def get_steepest_alpha(g, A):
-  return np.dot(g, g) / np.dot(g, np.dot(A, g))
+  p = -g
+  return -np.dot(g, p) / np.dot(p, np.dot(A, p))
 
-# ------------------------>
-
-epsilon = 0.01
-N = 10
-X = np.array([-10.0, -10.0, -10.0])
-
-def steepest_gradient_descent(X):
+def steepest_gradient_descent(X, epsilon = 0.01):
   "Метод наискорейшего градиентного спуска"
-  for iteration in range(1000):
+  for iteration in range(10):
     current_f = f(*X)
     current_g = gradient(*X).flatten()
 
-    print(f"Итерация {iteration}: X={X}, f(X)={current_f}, g(X)={current_g}")
+    print(f"Итерация {iteration+1}: X={X}, f(X)={current_f}, g(X)={current_g}")
 
     if np.linalg.norm(current_g) <= epsilon:
       print(f"Сходимость достигнута!")
-      print(f"Точка минимума: {X}")
-      print(f"Значение функции: {current_f:.6f}")
-      eigenvalues = np.linalg.eigvals(hessian)
-      min_l, max_l = np.min(eigenvalues), np.max(eigenvalues)
-      print(f"Собственные значения: min={min_l:.2f}, max={max_l:.2f}, отношение={min_l/max_l:.4f}")
       break
 
     alpha = get_steepest_alpha(current_g, hessian)
 
     X -= alpha * current_g
+  else:
+    print("Достигнуто максимальное количество итераций")
+  print(f"Точка минимума: {X}")
+  print(f"Значение функции: {current_f:.6f}")
+  eigenvalues = np.linalg.eigvals(hessian)
+  min_l, max_l = np.min(eigenvalues), np.max(eigenvalues)
+  print(f"Собственные значения: min={min_l:.2f}, max={max_l:.2f}, отношение={min_l/max_l:.4f}")
 
+X = np.array([10, 10, 10], dtype=float)
 steepest_gradient_descent(X)
